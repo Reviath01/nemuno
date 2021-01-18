5ca313c01f9267bc82e1690190c2d6a244b1f8d0
Reviath <28-OnurAkdikan@users.noreply.git.randomchars.net>
2021-01-18 15:20:34 +0000

    added unmute command

---
+++ commands/unmute.js
@@ -0,0 +1,27 @@
+
+const Discord = require('discord.js');
+const ms = require("ms");
+
+exports.run = async(client, message, args, connection) => {
+
+    if (message.member.hasPermission("MANAGE_ROLES")){
+
+    let spammer = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let role = message.guild.roles.cache.find(r => r.name === "Muted");
+    if(!role) return;
+
+    if (!spammer.roles.cache.has(role.id)) return message.channel.send('User is not muted.');
+    spammer.roles.remove(role.id);
+    message.channel.send(`<@${spammer.id}> can speak again.`);
+    }
+
+    else {
+        message.channel.send(`Lacking permission to perform such action.`)
+    }
+    
+};
+
+module.exports.help = {
+    name: 'unmute',
+    aliases: ['um']
+};
