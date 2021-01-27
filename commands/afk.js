5504dbb325a095939bda21e2aabe54a5ae6a2a99
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-01-27 11:58:31 +0000

    added reason for AFK

--- commands/afk.js
+++ commands/afk.js
@@ -2,7 +2,12 @@ const Discord = require('discord.js');
 const db = require('quick.db')
 exports.run = async (client, message, args) => {
  db.set(message.author.id + '.afk', 'true')
- message.channel.send(`<@${message.author.id}> You are now afk`)
+ let afk = args.join(' ');
+ if(!afk) {
+   afk = "AFK"
+ }
+ db.set(message.author.id + '.afkreason', `${afk}`)
+ message.channel.send(`<@${message.author.id}> You are now AFK with reason: ${afk}`)
 }
       module.exports.help = {
         name: 'afk',
