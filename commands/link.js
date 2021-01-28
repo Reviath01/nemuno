40c1a661577ec1565dcb43be0e6b7536b21bf6c1
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-01-28 11:28:24 +0000

    Update commands/link.js

--- commands/link.js
+++ commands/link.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 
 exports.run = async(client, message, args) => {
-    if(!message.author.hasPermission("ADMINISTRATOR")) return message.channel.send('You need administrator permission to use this command!')
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('You need administrator permission to use this command!')
 let a = args.join(' ');
 if(!a) return message.channel.send('Correct usage: -link {allow or deny}');
 if(a == "deny") {
