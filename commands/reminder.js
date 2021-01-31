5f600329d8122cdc7ed98448ccc8d137a4275909
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-01-31 06:49:33 +0000

    Update commands/reminder.js

--- commands/reminder.js
+++ commands/reminder.js
@@ -5,7 +5,10 @@ exports.run = async (client, message, args) => {
     let mesaj = args.slice(1).join(' ')
 message.channel.send(`I will remind you in ${time}`)
     setTimeout(function () {
-      message.channel.send(`Reminder for <@${message.author.id}> \n\`${mesaj}\``);
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`Reminder for <@${message.author.id}> \n\`${mesaj}\``)
+        .setColor('RANDOM')
+      message.channel.send(embed);
     }, ms(time));
 }
     module.exports.help = {
