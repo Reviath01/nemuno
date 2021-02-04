ea418cc5ec9116fbbcefd89ffb8ee65abc0baa74
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-02-04 10:42:06 +0000

    Update commands/server-settings.js

--- commands/server-settings.js
+++ commands/server-settings.js
@@ -28,6 +28,28 @@ if(linkblock === null) {
 if(linkblock === "deny"){
     linkblock = "Blocked"
 }
+let autorole = db.get(`role_${message.guild.id}`)
+if(autorole === null){
+    autorole = "Not Setted"
+}
+let autorole2;
+if(autorole == "Not Setted"){
+  autorole2 = "Not Setted"
+}
+else {
+    autorole2 = `<@&${autorole}>`
+}
+let modlog = db.get(`log_${message.guild.id}`)
+if(modlog == null){
+    modlog = "Not Setted"
+}
+let modlog2;
+if(modlog == "Not Setted") {
+    modlog2 = "Not Setted"
+}
+else {
+    modlog2 = `<#${modlog}>`
+}
 
 const embed = new Discord.MessageEmbed()
 .setAuthor(message.author.username, message.author.avatarURL())
@@ -39,6 +61,8 @@ const embed = new Discord.MessageEmbed()
 .addField("Goodbye Message", `${goodbyemessage}`)
 .addField("Level Message", `${lvlmsg}`)
 .addField("Link Block", `${linkblock}`)
+.addField("Autorole", `${autorole2}`)
+.addField("Mod-log", `${modlog2}`)
 .setColor('RANDOM')
 message.channel.send(embed)
 };
