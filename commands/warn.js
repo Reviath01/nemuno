78c6a7f1801cbe18d1748cdb6ca45063a33b9c67
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-02-04 06:41:03 +0000

    Update commands/warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -4,16 +4,16 @@ exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
   let reason = args.slice(1).join(' ');
   if (!user) return message.channel.send("Who are you warning?");
-  if(user.id == message.author.id) return message.channel.send('You can\'t warn yourself');
+  if(user == message.author) return message.channel.send('You can\'t warn yourself');
   if (!reason) return message.channel.send("Why are you warning");
-  message.channel.send('I send your message')
   const embed = new Discord.MessageEmbed()
   .setColor('RANDOM')
-  .setTitle(`You have been warned by <@${message.author.id}>`)
+  .setDescription(`You have been warned by <@${message.author.id}>`)
   .setTimestamp()
   .addField('Warn reason:', reason)
   .setFooter(client.user.username, client.user.avatarURL())
    user.send(embed)
+message.channel.send('I send your message')
 };
 module.exports.help = {
     name: 'warn',
