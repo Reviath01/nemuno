04725cadfd233481433093279c0a2879df529ed9
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-02-10 13:48:38 -0500

    Fixed clear command

--- commands/clear.js
+++ commands/clear.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = function(client, message, args) {
-
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You need manage messages permission to run this command')
   let messagecount = parseInt(args.join(' '));
   if (!messagecount) return message.channel.send('How many messages do you want to delete')
   if (messagecount > 99) return message.channel.send(`I can't delete more than 99 messages!`);
